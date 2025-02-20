ab9b606837ed4a6d1354edf3a630012c860c2b71
Paulo-galvao <paulogalvaoj21@gmail.com>
2025-02-20 19:36:11 -0300

    front: navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+    return <>
+        <nav>
+            <div className="navbar-logo">
+                <span>FutNews</span>
+                <img src="/home/bola-logo.png" alt="bolinha" />
+            </div>
+            <div className="options">
+                <div className="navbar-login">
+                    <button className="login">Login
+                        <i class='bx bx-log-in'></i>
+                    </button>
+                 </div>
+                <div className="navbar-search">
+                    <input type="text" placeholder="Procure por um título" />
+                    <i class='bx bx-search-alt'></i>
+                </div>
+            </div>
+        </nav>
+    </>
+}
